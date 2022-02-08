5123435154a42d3c4a509e12258a4ce3805a3033
Casey Fazio <casefazio@gmail.com>
2022-02-07 20:23:30 -0700

    Add booleans

--- variables.rb
+++ variables.rb
@@ -9,3 +9,7 @@ oatmeal_per_person = -1000
 cost_per_pancake = 1.00
 cost_per_waffle = 2.50
 cost_per_oatmeal = 6.66
+
+pancake_goodness = true
+waffle_goodness = 16 == 12 + 4
+oatmeal_goodness = !true
