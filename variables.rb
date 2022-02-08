af07d06626e66b7a846f3767f477ddaccfabb639
Casey Fazio <casefazio@gmail.com>
2022-02-07 20:25:11 -0700

    Annotate 3 lines of code

--- variables.rb
+++ variables.rb
@@ -6,13 +6,13 @@ oatmeal = "disgusting"
 oatmeal = "why"
 
 pancakes_per_person = 5
-waffles_per_person = 2
+# waffles_per_person = 2
 oatmeal_per_person = -1000
 
-cost_per_pancake = 1.00
+# cost_per_pancake = 1.00
 cost_per_waffle = 2.50
 cost_per_oatmeal = 6.66
 
-pancake_goodness = true
+# pancake_goodness = true
 waffle_goodness = 16 == 12 + 4
 oatmeal_goodness = !true
